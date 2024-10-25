6822f533459adeb86baa3d0f3d2c6b26d6db5a44
Cuong Nguyen <cuong.shodan@gmail.com>
2024-10-25 20:34:40 +0200

    sort slider by date

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -7,18 +7,28 @@ import "./style.scss";
 const Slider = () => {
   const { data } = useData();
   const [index, setIndex] = useState(0);
+
+  /* 
+  Problem 1: The data is not sorted by date
   const byDateDesc = data?.focus.sort((evtA, evtB) =>
-    new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
+  new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
   );
+ */
+
+
+  const byDateDesc =
+    data?.focus.sort(
+      (evtA, evtB) => new Date(evtB.date) - new Date(evtA.date)
+    ) || [];
+
+
   const nextCard = () => {
-    setTimeout(
-      () => setIndex(index < byDateDesc.length ? index + 1 : 0),
-      5000
-    );
+    setTimeout(() => setIndex(index < byDateDesc.length ? index + 1 : 0), 5000);
   };
   useEffect(() => {
     nextCard();
   });
+
   return (
     <div className="SlideCardList">
       {byDateDesc?.map((event, idx) => (
